e01b7e174381086c7042960159d9b2cc963f7f6d
Adam Skoczylas <skocadam@gmail.com>
2019-03-25 06:57:57 -0400

    define model attributes

--- app/models/pokemon-details.js
+++ app/models/pokemon-details.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    name: DS.attr(),
+    height:DS.attr(),
+    weight: DS.attr(),
+    sprites: DS.attr()
 });
--- app/models/pokemon.js
+++ app/models/pokemon.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+    count: DS.attr(),
+    results: DS.attr()
 });
